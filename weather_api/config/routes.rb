ddcbcad4c388ef5f1c2f762839c21065d7bb1e53
natail <nataleanfuso@gmail.com>
2017-09-04 17:56:23 -0500

    reorganized folders

--- weather_api/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/' => 'forecasts#index'
-  get '/forecasts' => 'forecasts#index'
-
-end
