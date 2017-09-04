9031c13d8f197290e9c4af345637acebac083fea
natail <nataleanfuso@gmail.com>
2017-09-04 17:47:14 -0500

    added subtle background, destroy data over 1 hour old

--- weather_api/app/controllers/forecasts_controller.rb
+++ weather_api/app/controllers/forecasts_controller.rb
@@ -17,25 +17,12 @@ class ForecastsController < ApplicationController
             end
             @forecasts = Forecast.where(zip_code: @zip_code)    
 
-        # elsif Forecast.where("zip_code = ? and updated_at <= ?", @zip_code, DateTime.now - 1.hour).length != 0
-        #     @stale_forecasts = Forecast.where("zip_code = ? and updated_at <= ?", @zip_code, DateTime.now - 1.hour)
-
-        #     @response = Unirest.get("#{ ENV["API_HOST"]}/#{ ENV["API_TOKEN"] }/forecast/q/#{ @zip_code }.json").body["forecast"]["simpleforecast"]["forecastday"].first(3)
-
-        #     @stale_forecasts.each do
-        #         new_forecast = Forecast.update_attributes(
-        #                                     zip_code: @zip_code,
-        #                                     day: @response["date"]["pretty"],
-        #                                     high: @response["high"]["fahrenheit"],
-        #                                     low: @response["low"]["fahrenheit"],
-        #                                     description: @response["conditions"]
-        #                                     )
-        #         new_forecast.save
-        #     @forecasts = Forecast.where(zip_code: @zip_code)    
-        #     end
-
         else 
-            @forecasts = Forecast.where(zip_code: @zip_code)
+            @forecasts = Forecast.where(zip_code: @zip_code).last(3)
+            stale_forecasts = Forecast.where(zip_code: @zip_code).first(3)
+            stale_forecasts.each do |stale_forecast|
+                stale_forecast.destroy
+            end
         end
     end
 end
